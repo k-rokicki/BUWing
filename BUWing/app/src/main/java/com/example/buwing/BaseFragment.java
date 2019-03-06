74578a6f3c25a4969571d710e66d7b5873798499
mkozlowski98 <mk394389@students.mimuw.edu.pl>
2019-03-07 00:05:00 +0100

    [Maciek] metody z fragmentów w nadklasie

--- BUWing/app/src/main/java/com/example/buwing/BaseFragment.java
+++ BUWing/app/src/main/java/com/example/buwing/BaseFragment.java
@@ -1,8 +1,35 @@
 package com.example.buwing;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Objects;
 
 public class BaseFragment extends Fragment {
+
+    protected String title;
+    protected int _layout;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Objects.requireNonNull(getActivity()).setTitle(title);
+    }
+
     public void onBackPressed() {
+        Intent intent = new Intent(Objects.requireNonNull(getActivity()).getBaseContext(), LoggedInActivity.class);
+        startActivity(intent);
     }
 }
