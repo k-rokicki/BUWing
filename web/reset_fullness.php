15811d814943cceda73ff4cb8dd4a6a010dc49ee
Kacper Rokicki <k.rokicki@students.mimuw.edu.pl>
2019-04-15 09:24:23 +0200

    [Kacper] poprawka skryptu do resetowania zajętości

--- web/reset_fullness.php
+++ web/reset_fullness.php
@@ -1,24 +1,30 @@
 <?php
     $ini = parse_ini_file("database_credentials.ini");
+    $pass = parse_ini_file("reset_fullness_passphrase.ini");
 
     $login = $_GET['login'];
     $password = $_GET['password'];
+    $passphrase = $_GET['passphrase'];
 
-    $link = pg_connect("host=labdb dbname=bd user=" . $ini['db_user'] . " password=" . $ini['db_password']);
-    $result = pg_query($link,
-                        "SELECT password
-                        FROM admins
-                        WHERE login = '" . pg_escape_string($login) . "'");
-    $count = pg_num_rows($result);
+    if ($passphrase == $pass['passphrase']) {
 
-    if ($count == 1) {
-        $row = pg_fetch_array($result, 0);
-        $hashedPassword = $row["password"];
-        if (password_verify($password, $hashedPassword)) {
-            $result = pg_query($link,
-                        "UPDATE tables
-                        SET taken = FALSE");
+        $link = pg_connect("host=labdb dbname=bd user=" . $ini['db_user'] . " password=" . $ini['db_password']);
+        $result = pg_query($link,
+                            "SELECT password
+                            FROM users
+                            WHERE login = '" . pg_escape_string($login) . "'");
+        $count = pg_num_rows($result);
+
+        if ($count == 1) {
+            $row = pg_fetch_array($result, 0);
+            $hashedPassword = $row["password"];
+            if (password_verify($password, $hashedPassword)) {
+                $result = pg_query($link,
+                            "UPDATE tables
+                            SET taken = FALSE");
+            }
         }
+    
     }
 
     pg_close($link);
