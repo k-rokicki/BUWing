59fbc98dcf3eaca1a353be9791b353e2ef71f0c5
mkozlowski98 <mk394389@students.mimuw.edu.pl>
2019-04-26 23:58:47 +0200

    [Maciek] skrypt i layout bez wierszy

---
+++ web/friends_inside_info.php
@@ -0,0 +1,44 @@
+<?php
+
+  $ini = parse_ini_file("database_credentials.ini");
+
+  $link = pg_connect("host=labdb dbname=bd user=" . $ini['db_user'] . " password=" . $ini['db_password']);
+
+  $login = $_POST["login"];
+  $password = $_POST["password"];
+
+  $JSONobj->result = 0;
+
+  $result = pg_query($link, "SELECT id, password FROM users
+                            WHERE login =  '" . pg_escape_string($login) . "'");
+
+  if (pg_num_rows($result) == 1) {
+    $row = pg_fetch_array($result, 0);
+    $userId = $row["id"];
+    $hashedPassword = $row["password"];
+
+    if (password_verify($password, $hashedPassword)) {
+      $result = pg_query($link, "SELECT login, floor FROM tables LEFT JOIN users
+                                ON tables.userid = users.id
+                                WHERE userid = ANY(SELECT inviterid FROM friends
+                                WHERE inviteeid = $userId AND status = 't' UNION
+                                SELECT inviteeid FROM friends WHERE inviterid = $userId
+                                AND status = 't')");
+      $count = pg_num_rows($result);
+
+      for ($i = 0; $i < $count; $i++) {
+        $row = pg_fetch_array($result, $i, PGSQL_ASSOC);
+        $logins[] = $row;
+      }
+
+      if ($count > 0) {
+        $JSONobj->result = 1;
+        $JSONobj->friends = $logins;
+      }
+    }
+  }
+
+  pg_close($link);
+  $returnJSON = json_encode($JSONobj);
+  echo $returnJSON;
+?>
