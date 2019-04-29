9e129bb57cfdb88ea4d974e40a63fd813a85580a
mkozlowski98 <mk394389@students.mimuw.edu.pl>
2019-04-29 14:11:47 +0200

    [Maciek] usunięcie Log.d

--- BUWing/app/src/main/java/com/example/buwing/FriendsInsideFragment.java
+++ BUWing/app/src/main/java/com/example/buwing/FriendsInsideFragment.java
@@ -7,7 +7,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -106,7 +105,6 @@ public class FriendsInsideFragment extends BaseFragment {
                 try {
                     obj = new JSONObject(result);
                     success = Integer.parseInt(obj.get("result").toString());
-                    Log.d("inside info", "success: " + success);
                     if (success == 1) {
                         ArrayList<FriendRow> temp = new ArrayList<>();
                         array = obj.getJSONArray("friends");
@@ -119,7 +117,6 @@ public class FriendsInsideFragment extends BaseFragment {
                         }
                         friends.clear();
                         friends.addAll(temp);
-                        Log.d("inside info", "end of task");
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
