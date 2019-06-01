14f8bee5108f48792b0c380053c2e6c82eee9cbf
Joanna Ostrowska <Asia@MacBook-Pro-Asia.local>
2019-06-01 19:20:21 +0200

    [Joanna] funkcje z baza - nietestowane

--- BUWing/app/src/main/assets/main.js
+++ BUWing/app/src/main/assets/main.js
@@ -1,39 +1,62 @@
 var idStolik;
 var initWindowHeight = window.innerHeight;
 var initWindowWidth = window.innerWidth;
+var login, password, floor;
+var prevTable, prevTableFloor;
+
 
 // do sprawdzania ze przycisk odswieza
 var dt = new Date();
 document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
 
-
-    document.body.onclick = function(e) {   //when the document body is clicked
+    document.body.onclick = function(e) {
         if (window.event) {
-            e = event.srcElement;           //assign the element clicked to e (IE 6-8)
+            e = event.srcElement;
         }
         else {
-           e = e.target;                   //assign the element clicked to e
+           e = e.target;
         }
 
-        if (!e.className || e.className.indexOf('popup') == -1) { //zamykanie popupow gdy kliknie sie gdzie indziej
+        if (!e.className || e.className.indexOf('popup') == -1) {
             closePopups();
         }
 
         if (e.className && e.className.indexOf('stolik') != -1) {
             idStolik = e;
             if (e.status == "wolny") {  //obsluga wolnego
-                showPopupTT(); 
+                var postData = 'login=' + login + '&password=' + password; 
+                // console.log(postData);       
+                var http = new XMLHttpRequest();
+                var url = 'http://students.mimuw.edu.pl/~kr394714/buwing/seat_taken.php';
+                http.open('POST', url, true);
+
+                http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                http.send(postData);
+
+                http.onreadystatechange = function() {
+                    if(http.readyState == 4 && http.status == 200) {
+                        const content = JSON.parse(http.responseText);
+                        var result = content['taken'];
+                        if (!result) {
+                            showPopup(document.getElementById("popupZajmij"));
+                        }
+                        else {
+                            idStolik = document.getElementById(content['seatId']);
+                            showPopup(document.getElementById("popupInfo"));
+                        }
+                    }
+                }
             }
             else if (e.status == "okupowany") { //okupowany to stolik ktory zajmuje uzytkownik
-                showPopupFT();
+                showPopup(document.getElementById("popupZwolnij"));
             }
         }
-        var rect = e.getBoundingClientRect();
-        console.log(rect.top, rect.right, rect.bottom, rect.left);
+        //var rect = e.getBoundingClientRect();
+       // console.log(rect.top, rect.right, rect.bottom, rect.left);
       //  console.log(window.scrollY);
-      //  console.log(document.getElementById("stolik1").offsetLeft);
-        
+      //  console.log(document.getElementById("stolik1").offsetLeft);       
     }
+
     // pinch zoomowanie stolikow  
     document.getElementById("biblioteka").addEventListener('touchend', touchendeventListener, false);
 
@@ -50,61 +73,57 @@ document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
                 }
             }
     }
-    
-    
 
-    function showPopupTT() {
-        var windowHeight = window.innerHeight * 0.5; // zeby przy zoomie rozmiar popupu sie nie psul
-        var windowWidth = window.innerWidth * 0.5;
-        var font_size = window.innerHeight * 0.05;
-        console.log(windowHeight);
-        document.getElementById("popupZajmij").style.height = windowHeight + "px";
-        document.getElementById("popupZajmij").style.width = windowWidth + "px";
-
-        var marginLeft = window.scrollX + window.innerWidth/4;
-        var marginTop = window.scrollY + window.innerHeight/4;
-
-        document.getElementById("popupZajmij").style.left = marginLeft + "px";
-        document.getElementById("popupZajmij").style.top = marginTop + "px";
-
-        var popupButtons = document.getElementsByClassName('popupButton');
-        popupButtons[0].style.fontSize = font_size + "px";
-        /*for (let i = 0; i < popupButtons.length; i++) {
-            popupButtons[i].style.fontSize = font_size + "px";
-        }*/
-        document.getElementById("popupZajmij").style.display = "block";
-    }
-
-    function showPopupFT() {
+    // pokazywanie popupow
+    function showPopup(popup) {
         var windowHeight = window.innerHeight * 0.5;
         var windowWidth = window.innerWidth * 0.5;
         var font_size = window.innerHeight * 0.05;
-        document.getElementById("popupZwolnij").style.height = windowHeight + "px";
-        document.getElementById("popupZwolnij").style.width = windowWidth + "px";
+
+        popup.style.height = windowHeight + "px";
+        popup.style.width = windowWidth + "px";
 
         var marginLeft = window.scrollX + window.innerWidth/4;
         var marginTop = window.scrollY + window.innerHeight/4;
 
-        document.getElementById("popupZwolnij").style.left = marginLeft + "px";
-        document.getElementById("popupZwolnij").style.top = marginTop + "px";
+        popup.style.left = marginLeft + "px";
+        popup.style.top = marginTop + "px";
         var popupButtons = document.getElementsByClassName('popupButton');
-        popupButtons[1].style.fontSize = font_size + "px";
-        /*for (let i = 0; i < popupButtons.length; i++) {
+        for (let i = 0; i < popupButtons.length; i++) {
             popupButtons[i].style.fontSize = font_size + "px";
-        }*/
-        document.getElementById("popupZwolnij").style.display = "block";
+        }
+
+        popup.style.display = "block";
     }
 
+    // zamykanie popupow
     function closePopups() {
         document.getElementById("popupZajmij").style.display = "none";
         document.getElementById("popupZwolnij").style.display = "none";
     }
 
-    function freeTable() {
-        if (successfulFT()) {
-            idStolik.style.background = "#28724F";
-            idStolik.status = "wolny";
-        }
+    // zwalnianie stolika
+    function freeTable() {  
+        var postData = 'login=' + login + '&password=' + password; 
+       // console.log(postData);       
+        var http = new XMLHttpRequest();
+        var url = 'http://students.mimuw.edu.pl/~kr394714/buwing/release_seat.php';
+        http.open('POST', url, true);
+
+        http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        http.send(postData);
+
+        http.onreadystatechange = function() {
+            if(http.readyState == 4 && http.status == 200) {
+                const content = JSON.parse(http.responseText);
+                console.log(content);  
+                var result =  content[released];          
+                if (result && idStolik) {
+                    idStolik.style.background = "#28724F";
+                    idStolik.status = "wolny";
+                }
+            }
+        } 
         closePopups();
     }
 
@@ -133,7 +152,7 @@ document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
         http.onreadystatechange = function() {
             if(http.readyState == 4 && http.status == 200) {
                 const content = JSON.parse(http.responseText);
-               // console.log(content['took']);
+                console.log(content['took']);
                 var result = content['took'];
                 if (result == 1) {
                     idStolik.style.background = "orange";
@@ -144,15 +163,45 @@ document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
         closePopups();
     }
 
-    //laczenie z baza czy udalo sie zwolnic
-    function successfulFT() {
-        return true;
-    }
-
-    var login, password, floor;
 
     //przypisze statusy stolikom na podstawie skryptu php przy zaladowaniu strony
     function assignTableStatus() {
+        /*
+        var postData = 'login=' + login + '&password=' + password + '&floor=' + floor; 
+       // console.log(postData);       
+        var http = new XMLHttpRequest();
+        var url = 'http://students.mimuw.edu.pl/~kr394714/buwing/tables_status_info.php';
+        http.open('POST', url, true);
+
+        http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        http.send(postData);
+
+        http.onreadystatechange = function() {
+            if(http.readyState == 4 && http.status == 200) {
+                const content = JSON.parse(http.responseText);
+               // console.log(content['took']);
+                for (let i in content['tables']) {
+                    var table = document.getElementById(i[0]);
+                    if (table != null) {
+                        if (i[1] == true) {
+                            if (i[2] == login) {
+                                table.status = "okupowany";
+                                table.style.background = "orange";
+                            }
+                            else {
+                                table.status = "zajety";
+                                table.style.background = "#C86BA8";
+                            }                           
+                        }
+                        else {
+                            table.status = "wolny";
+                            table.style.background = "#28724F";
+                        }
+                    }
+                }
+            }
+        } */
+
         document.getElementById("stolik1").status = "wolny";
         document.getElementById("stolik2").status = "wolny";
 
